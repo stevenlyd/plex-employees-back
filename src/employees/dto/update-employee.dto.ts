a6f02accf7aa2f641852b1539e8d992f575f9c19
Steven Liu <liuyidong9@gmail.com>
2023-04-03 14:28:51 -0400

    Initialized employees module/service.

---
+++ src/employees/dto/update-employee.dto.ts
@@ -0,0 +1,4 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { CreateEmployeeDto } from './create-employee.dto';
+
+export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
